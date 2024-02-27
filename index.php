7710a8ebcc02345bbee2985ea32a7b9095e00402
leonardoolia <leonardooliaa@gmail.com>
2024-02-27 13:31:20 +0100

    printed products on page

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-
+require_once __DIR__ . '/data/products.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <ul>
+        <?php foreach ($products as $product):?>
+            <li><?=$product->getProductDetails() ?></li>
+        <?php endforeach;?>
+    </ul>
+</body>
+</html>
